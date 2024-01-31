8b35630ba46298cf4196640fe1a6028e8391ae16
sandeepmore-code <152766192+sandeepmore-code@users.noreply.github.com>
2024-01-31 16:10:25 +0530

    update

---
+++ Day1/day1.js
@@ -0,0 +1,15 @@
+//datatypes:
+
+//information
+//name
+//age
+//number
+//yes / no boolean
+
+// string - "", '' ,``
+// "true"
+// 'awdiz'
+// '1234'
+
+// predefined function - set of code
+console.log("Awdiz6")
