c73ee74e05aa11b5223e757f2ae46e32b088ac12
sandeepmore-code <152766192+sandeepmore-code@users.noreply.github.com>
2024-02-03 03:08:31 +0530

    updates

--- Day1/day1.js
+++ Day1/day1.js
@@ -21,4 +21,74 @@ console.log(`Awdiz`)
 console.log(typeof(`12.13`))
 
 console.log("ftyvbg" , typeof(`igosty`) , "87655jbjdbjf" , "hfasidahil")
-console.log(2+2) 
+console.log(2+2)
+
+Number
+
+console.log(1234, typeof(1234))
+console.log('1234' , typeof('1234'))
+
+console.log(1+1)
+console.log("1" + '1')
+
+Boolean
+true,false
+
+console.log(true , typeof ('true'))
+console.log(false, typeof(false))
+console.log(true , typeof(true))
+
+
+// variables
+
+// data store
+
+// Var
+// let
+// const
+
+var instituteName = "Awdiz"
+var age = 10
+var isOpen = true
+
+console.log(instituteName,age,isOpen)
+
+
+var value1 = 10;
+var value2 = 20;
+
+console.log(value1+value2)
+console.log(value1-value2)
+console.log(value1 * value2)
+
+var kuchbhi = 12;
+
+console.log(kuchbhi)
+console.log(kuchbhi)
+console.log(kuchbhi)
+console.log(kuchbhi)
+
+var myValue = 10;
+console.log(myValue)
+ var myvalue=20; 
+ console.log(myvalue)
+ myvalue = myvalue +10
+ var myvalue = 50
+ console.log(myvalue)
+
+const kuchbhi = 1234
+kuchbhi = 29
+const kuchbhi = 34567 
+console.log(kuchbhi)
+
+let kuchbhi = 1234;
+kuchbhi = 2345;
+let kuchbhi = 3455
+console.log (kuchbhi)
+
+
+
+
+
+
+
