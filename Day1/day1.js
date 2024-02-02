9a7502ba448d964877957da0987d07d72b5f39dd
sandeepmore-code <152766192+sandeepmore-code@users.noreply.github.com>
2024-02-02 23:56:17 +0530

    updates

--- Day1/day1.js
+++ Day1/day1.js
@@ -14,3 +14,11 @@
 // predefined function - set of code
 
 console.log("Awdiz6")
+console.log(typeof("pass@1234"))
+console.log('Awdiz')
+console.log(typeof('true'))
+console.log(`Awdiz`)
+console.log(typeof(`12.13`))
+
+console.log("ftyvbg" , typeof(`igosty`) , "87655jbjdbjf" , "hfasidahil")
+console.log(2+2) 
