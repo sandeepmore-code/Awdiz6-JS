88469a485a085dab9009e4d0fea0dd914f3b1dbf
sandeepmore-code <152766192+sandeepmore-code@users.noreply.github.com>
2024-02-16 14:50:34 +0530

    updates

---
+++ day9/day9.js
@@ -0,0 +1,9 @@
+setTimeout(() => {
+    console.log("Excuted after 2000 msce.")
+    console.log("Excuted after 2000 msce.")
+    console.log("Excuted after 2000 msce.")
+}, 5000);
+
+const MyFunction = (value1, value2) => {
+    return value1 * value2
+}
