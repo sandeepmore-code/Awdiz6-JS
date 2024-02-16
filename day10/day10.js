4377b63ea7e4d337ec7c831ac6f0163caecd9f60
sandeepmore-code <152766192+sandeepmore-code@users.noreply.github.com>
2024-02-16 23:08:10 +0530

    updates

---
+++ day10/day10.js
@@ -0,0 +1,48 @@
+// Results are in Boolean in Map funtion
+
+// var array = [1,8,2,3,4,5,6]
+// const result = array.map((num)=>num < 5)
+// console.log(result)
+
+
+
+// foreach proviedds each elment seperatly as it is
+// var array1 = [1,2,3,4,5,6,7]
+// const result= array1.forEach((element)=> console.log (element)) 
+
+// push
+// var Animal= [ "Dog","Tiger" ,"Cat" ]
+// const result=Animal.push("Lion")
+// console.log(result)
+// console.log(Animal)
+
+// var Player = ["Virat", "Rahul", "Ravindra"]
+// const Result= Player.push("Rohit")
+// console.log(add , "add")
+// console.log(Player)
+
+// Pop
+// Pull iteam from last position.
+// var Sandeep=[1,2,3,4,5]
+// const Result = Sandeep.pop()
+// console.log(Result)
+// console.log(Sandeep)
+
+// // shift
+
+// Removes value form strating
+
+// var country = ["England", "India","Shrilanka"]
+// const result = country.shift()
+// console.log(result)
+// console.log(country)
+
+// unshift
+// Add value in starting/begining of array.
+// var Fruits= ["Apple", "Mango", "Banana"]
+// const Result = Fruits.unshift("AKNDdk")
+// console.log(Result)
+// console.log(Fruits)
+
+
++
